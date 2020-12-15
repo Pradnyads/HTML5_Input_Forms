b4141671ce95b55d09afcb4966d2f803c989b53b
Pradnyads <sorepradnya@gmail.com>
2020-12-15 20:42:02 +0530

    [Pradnya]Add: Name validation for input forms

---
+++ JavaScript/inputForms.js
@@ -0,0 +1,19 @@
+//UC1
+//Name starts with capital letter and minimum 3 letters
+
+const text = document.querySelector("#text");
+const textError = document.querySelector(".text-error");
+text.addEventListener("input", function() {
+  let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+  if (nameRegex.test(text.value)) textError.textContent = "";
+  else textError.textContent = "Name is Incorrect";
+});
+
+//Salary range button synchronised with values of user
+
+const salary = document.querySelector("#salary");
+const output = document.querySelector(".salary-output");
+output.textContent = salary.value;
+salary.addEventListener("input", function() {
+  output.textContent = salary.value;
+});
