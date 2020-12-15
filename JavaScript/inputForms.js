552fc0514e577d036811e4d8cf0a45ba2fe73db1
Pradnyads <sorepradnya@gmail.com>
2020-12-15 21:01:20 +0530

    [Pradnya]Refactor : Email validatio

--- JavaScript/inputForms.js
+++ JavaScript/inputForms.js
@@ -1,12 +1,27 @@
-//UC1
+//UC1 - Name Validation
 //Name starts with capital letter and minimum 3 letters
 
 const text = document.querySelector("#text");
 const textError = document.querySelector(".text-error");
-text.addEventListener("input", function() {
+text.addEventListener("input", function () {
   let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
   if (nameRegex.test(text.value)) textError.textContent = "";
   else textError.textContent = "Name is Incorrect";
+});
+
+//UC2 -Email Validation
+
+const email = document.querySelector("#email");
+const emailError = document.querySelector(".email-error");
+email.addEventListener("input", function () {
+  let emailRegex = RegExp(
+    "^[A-Za-z]{3,}([.+_-][A-Za-z0-9]+)*[@][A-Za-z0-9]{1,}[.][a-zA-Z0-9]{2,3}([.][0-9a-zA-Z]{2})?$"
+  );
+  if (emailRegex.test(email.value)) {
+    emailError.textContent = "";
+  } else {
+    emailError.textContent = "Email is not Valid";
+  }
 });
 
 //Salary range button synchronised with values of user
@@ -14,6 +29,6 @@ text.addEventListener("input", function() {
 const salary = document.querySelector("#salary");
 const output = document.querySelector(".salary-output");
 output.textContent = salary.value;
-salary.addEventListener("input", function() {
+salary.addEventListener("input", function () {
   output.textContent = salary.value;
 });
